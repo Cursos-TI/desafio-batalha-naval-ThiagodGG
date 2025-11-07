4ab085972c9897078aa531b97ce0399f5d96c811
Thiago Barboza <thiagoafonsobarboza@gmail.com>
2025-11-07 17:54:09 +0000

    Feito o desafio novato, posicionei dois navios no tabuleiro do jeito que eu entendi

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,14 +1,63 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define TAM 10       // Tamanho do tabuleiro 10x10 bidimensional
+#define TAM_NAVIO 3  // Tamanho fixo dos navios (3 posições)
+#define AGUA 0
+#define NAVIO 3
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+   
+    int tabuleiro [TAM][TAM]; //Matriz do tabuleiro
+    int i, j;
+
+    // Inniciando o tabuleiro com a água = 0
+    for (i = 0; i < TAM; i++){
+        for (j = 0; j < TAM; j++){
+            tabuleiro[i][j] = AGUA;
+        }
+    }
+
+    //Coordenadas dos navios
+    int linNavioHorz = 2;
+    int colNavioHorz = 4;
+    int linNavioVert = 5;
+    int colNavioVert = 7;
+
+    // --- Validação dos limites do tabuleiro ---
+    // Verifica se o navio horizontal cabe no tabuleiro
+    if (colNavioHorz + TAM_NAVIO > TAM) {
+        printf("Erro: o navio horizontal ultrapassa os limites do tabuleiro.\n");
+        return 1;
+    }
+
+    // Navio Horizontal
+    for (i = 0; i < TAM_NAVIO; i++) {
+        tabuleiro[linNavioHorz][colNavioHorz + i] = NAVIO;
+    }
+
+    for (i = 0; i < TAM_NAVIO; i++) {
+        if (tabuleiro[linNavioVert + i][colNavioVert] == NAVIO) {
+            printf("Erro: os navios se sobrepõem no tabuleiro!\n");
+            return 1;
+        }
+    }
+
+    // Navio Vertical
+     for (i = 0; i < TAM_NAVIO; i++) {
+        tabuleiro[linNavioVert + i][colNavioVert] = NAVIO;
+    }
+
+    // Tabuleiro 
+    printf("\n=== TABULEIRO DE BATALHA NAVAL ===\n\n");
+    for (i = 0; i < TAM; i++) {
+        for (j = 0; j < TAM; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nLegenda: 0 = Água | 3 = Navio\n");
+    printf("\n");
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
